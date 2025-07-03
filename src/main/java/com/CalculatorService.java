4dbb5a01f9c671c6b15c91f291cc92b28f058e63
Vamsi <vamsi.chava@wissen.com>
2025-07-03 17:03:56 +0530

    TESTING

--- src/main/java/com/CalculatorService.java
+++ src/main/java/com/CalculatorService.java
@@ -5,7 +5,7 @@ public class CalculatorService {
 	public static void main(String[] args) {
 		Calculator cal = new Calculator();
 		cal.add(10, 20);
-		System.out.println("ok-------------------------Working");
+		System.out.println("ok-------------------------");
 	}
 
 }
