4d8cfe45289a34ee9b1463518caefa69a94a1281
Vamsi <vamsi.chava@wissen.com>
2025-07-04 10:01:07 +0530

    Result in console output2

--- src/main/java/com/CalculatorService.java
+++ src/main/java/com/CalculatorService.java
@@ -5,7 +5,7 @@ public class CalculatorService {
 	public static void main(String[] args) {
 		Calculator cal = new Calculator();
 		int result = cal.add(10, 20);
-		System.out.println("%%%%%%%%%%%%%%%% Add Result: "+result+" %%%%%%%%%%%%%%%%%%%%%%%%");
+		System.out.println("%%%%%%%%%%%%%%%%- Add Result: "+result+" %%%%%%%%%%%%%%%%%%%%%%%%");
 	}
 
 }
